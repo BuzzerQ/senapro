7550fab2d54214e795380b7c43553685ae9ec7db
BuzzerQ <abrahamyoel9@gmail.com>
2026-08-27 12:21:28 +0700

    favicon

---
+++ global.js
@@ -0,0 +1,5 @@
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.type = 'image/png';
+favicon.href = 'assets/images/iconsena.png'; // arahkan ke path logo kamu
+document.head.appendChild(favicon);
